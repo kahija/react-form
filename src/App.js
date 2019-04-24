9ad27325873ed62c221a0f376a41e53639ee05ba
kahija <ouarezki.kahina@gmail.com>
2019-04-24 11:17:46 +0200

    react form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormEmployee from './FormEmployee'
+class App extends Component {
+  render() {
+    return (
+      <FormEmployee/>
+    )
+  }
+}     
+
+
+export default App;
